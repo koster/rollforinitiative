a1278ecb77e59d7c798163a3a36dfc082b107d70
Constantine Suzev <suzeff@gmail.com>
2016-02-25 21:12:58 +0300

    Monster initiative bar

--- src/config.js
+++ src/config.js
@@ -7,27 +7,18 @@
         {
             name: 'Thurandil',
             initiative: 5,
-            enemy: false
         },
         {
             name: 'Gilian',
             initiative: 6,
-            enemy: false
         },
         {
             name: 'Targor',
             initiative: 1,
-            enemy: false
         },
         {
             name: 'Brakzar',
             initiative: 3,
-            enemy: false
-        },
-        {
-            name: 'Enemy',
-            initiative: main.DefaultInitiative,
-            enemy: true
         }
     ]
 })(window)
